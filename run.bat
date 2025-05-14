7b5a9b022eecc3d3c57c3e92c1b7b3fd1078358e
shoaib <shaik.shoaib.70@gmail.com>
2025-05-14 17:23:42 +0530

    run.bat file update

--- run.bat
+++ run.bat
@@ -1,14 +1,12 @@
 @echo off
 
 :: Activate the virtual environment
-call C:\Users\shaik\PycharmProjects\Selenium_Python_Demo\.venv\Scripts\activate.bat
+call venv\Scripts\activate.bat
 
-:: Run pytest with the desired options
+:: Optional: print Python location
+where python
+
+:: Run the tests
 pytest -v -s -m "sanity" --html=Reports/report.html testCases/ --browser chrome
 
-:: Pause to keep the console window open
 pause
-
-rem pytest -v -s -m 'regression' --html=Reports/report.html testCases/
-rem pytest -v -s -m 'sanity and regression' --html=Reports/report.html testCases/
-rem pytest -v -s -m 'sanity or regression' --html=Reports/report.html testCases/
